5a60276bf8f9881a5b8c4b98a8db2116a8484c2b
Gabrielapassos2008 <gabrielpassos2008@email.com>
2026-01-08 22:36:34 -0300

    adicionei o metedo que ira testar a idade, a nota e o nome

--- src/colegio/Aluno.java
+++ src/colegio/Aluno.java
@@ -14,7 +14,6 @@ public class Aluno {
         this.nota2 = nota2;
         this.nota3 = nota3;
     }
-    
     public void mostrarDadosCompletos(){
         System.out.println("nome do aluno:" + this.nome);
         System.out.println("idade do aluno:" + this.idade );
@@ -22,4 +21,46 @@ public class Aluno {
         System.out.println("nota do segundo trimestre" + this.nota2);
         System.out.println("nota do terceiro trimestre" + this.nota3);
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(float nota1) {
+        this.nota1 = nota1;
+    }
+
+    public float getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(float nota2) {
+        this.nota2 = nota2;
+    }
+
+    public float getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(float nota3) {
+        this.nota3 = nota3;
+    }
+
+
 }
